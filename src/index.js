e56e8bbced55717c6baeb43cd2d13e82bebdc20b
RedJanvier <janvierntwali@gmail.com>
2020-11-12 17:55:34 +0200

    feat(node): setup empty initial app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express, { json } from 'express';
+import { serve, setup } from 'swagger-ui-express';
+import docs from '../docs/index.json';
+
+const { PORT } = process.env;
+const app = express();
+
+app.use(json());
+app.use('/', serve, setup(docs));
+app.get('/api', (req, res) => res.sendStatus(200));
+
+app.listen(PORT, console.log(`Server is started at http://localhost:${PORT}`));
+
+export default app;
