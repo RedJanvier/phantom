336935874f5314272d44256553925879663bf804
RedJanvier <janvierntwali@gmail.com>
2020-11-14 19:08:16 +0200

    chore(cicd): setup travis & coveralls
    - setup tests env
    - setup travisCI with mocha and chai
    - setup coveralls with nyc for coverage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import '@babel/polyfill';
+import morgan from 'morgan';
+import helmet from 'helmet';
 import express, { json } from 'express';
 import { serve, setup } from 'swagger-ui-express';
 import docs from '../docs/index.json';
@@ -6,6 +9,8 @@ const { PORT } = process.env;
 const app = express();
 
 app.use(json());
+app.use(helmet());
+app.use(morgan('dev'));
 app.use('/', serve, setup(docs));
 app.get('/api', (req, res) => res.sendStatus(200));
 
