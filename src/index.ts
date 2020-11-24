153e3ab23a6a654e915601f957bb15369520e7f8
RedJanvier <janvierntwali@gmail.com>
2020-11-24 05:19:08 +0200

    chore(typescript): setup types
    - setup project with tests

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-import '@babel/polyfill';
+import 'dotenv/config.js';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import express, { json } from 'express';
+import express from 'express';
 import { serve, setup } from 'swagger-ui-express';
-import docs from '../docs/index.json';
+import docs from './docs/index.json';
 
 const { PORT } = process.env;
 const app = express();
 
-app.use(json());
+app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use('/', serve, setup(docs));
 app.get('/api', (req, res) => res.sendStatus(200));
 
-app.listen(PORT, console.log(`Server is started at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is started at http://localhost:${PORT}`)
+);
 
 export default app;
